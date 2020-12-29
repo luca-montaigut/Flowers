c13a340f26c01fd93f9b8a002fc89f518ce7be4a
Luca Montaigut <lmontaigut@gmail.com>
2020-12-29 03:01:44 +0100

    add easter egg

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const angle = document.getElementById("angle");
 const step = document.getElementById("step");
 const scale = document.getElementById("scale");
 const limit = document.getElementById("limit");
+const draw = document.getElementById("draw");
+const lazyrabbit = document.getElementById("lazyrabbit");
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
@@ -49,3 +51,51 @@ form.addEventListener("submit", (e) => {
   number = 0;
   animate();
 });
+
+// Easter egg
+function randomFlower() {
+  angle.value = (Math.random() * (0.1 - 10) + 10).toFixed(1);
+  step.value = Math.floor(Math.random() * 10) + 1;
+  scale.value = Math.floor(Math.random() * (15 - 5)) + 5;
+  limit.value = Math.floor(Math.random() * (350 - 150)) + 150;
+  draw.click();
+}
+
+const toast = {
+  timer: null,
+
+  show: function (message) {
+    document.getElementById("toast-box").innerHTML = message;
+    document.getElementById("toast-box").style.display = "block";
+
+    if (toast.timer != null) {
+      clearTimeout(toast.timer);
+      toast.timer = null;
+    }
+
+    toast.timer = setTimeout(toast.hide, 1500);
+  },
+
+  hide: function () {
+    document.getElementById("toast-box").style.display = "none";
+    clearTimeout(toast.timer);
+    toast.timer = null;
+  },
+};
+
+lazyrabbit.addEventListener(
+  "click",
+  () => {
+    toast.show("Random Mode unlocked!");
+    setTimeout(() => {
+      draw.insertAdjacentHTML(
+        "afterend",
+        "<hr><input type='submit' value='Random Mode' id='random'>"
+      );
+      const random = document.getElementById("random");
+      random.addEventListener("click", randomFlower);
+      lazyrabbit.style.display = "none";
+    }, 1500);
+  },
+  { once: true }
+);
